12d877cdf743f67bbd07b7129913b10671e4fdec
郭明锋 <gmf520@yeah.net>
2017-08-23 11:39:16 +0800

    将demo项目调整到test中

---
+++ tests/web/OSharp.Demo.Web/Startups/DesignTimeDefaultDbContext.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using OSharp.Entity;
+
+namespace OSharp.Demo.Web
+{
+    [OSharp.Dependency.IgnoreDependency]
+    public class DesignTimeDefaultDbContext : DefaultDbContext
+    {
+        public DesignTimeDefaultDbContext(DbContextOptions options, IEntityConfigurationTypeFinder typeFinder)
+            : base(options, typeFinder)
+        {
+        }
+    }
+}
