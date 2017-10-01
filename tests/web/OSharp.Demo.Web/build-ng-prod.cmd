11e2b919688b8995aa3c2411965ea75fe878d7d8
郭明锋 <mf.guo@qq.com>
2017-10-01 12:07:46 +0800

    1

---
+++ tests/web/OSharp.Demo.Web/build-ng-prod.cmd
@@ -0,0 +1,3 @@
+node node_modules/webpack/bin/webpack.js --config webpack.config.vendor.js --env.prod
+node node_modules/webpack/bin/webpack.js --env.prod
+pause
---
+++ tests/web/OSharp.Demo.Web/build-ng.cmd
@@ -0,0 +1,3 @@
+node node_modules/webpack/bin/webpack.js --config webpack.config.vendor.js
+node node_modules/webpack/bin/webpack.js
+pause
