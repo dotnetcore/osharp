ff8c85337fbfde865f0e03642abec88837dc9e5c
郭明锋 <gmf520@yeah.net>
2018-07-10 12:27:32 +0800

    重新整理前台UI，计划使用Material2

--- tests/web/ui/ng-alain/src/app/layout/layout.module.ts
+++ tests/web/ui/ng-alain/src/app/layout/layout.module.ts
@@ -2,23 +2,10 @@ import { NgModule } from '@angular/core';
 import { SharedModule } from '@shared/shared.module';
 import { LayoutModule as CdkLayoutModule } from '@angular/cdk/layout';
 import { LayoutDefaultComponent } from './default/default.component';
-import { HeaderComponent } from './default/header/header.component';
-import { HeaderFullScreenComponent } from './default/header/components/fullscreen.component';
-import { HeaderIconComponent } from './default/header/components/icon.component';
-import { HeaderNotifyComponent } from './default/header/components/notify.component';
-import { HeaderStorageComponent } from './default/header/components/storage.component';
-import { HeaderUserComponent } from './default/header/components/user.component';
 
 const COMPONENTS = [
   LayoutDefaultComponent,
 
-  // header
-  HeaderComponent,
-  HeaderFullScreenComponent,
-  HeaderIconComponent,
-  HeaderNotifyComponent,
-  HeaderStorageComponent,
-  HeaderUserComponent,
 ];
 
 @NgModule({
--- tests/web/ui/ng-alain/src/app/routes/admin/layout/layout.module.ts
+++ tests/web/ui/ng-alain/src/app/routes/admin/layout/layout.module.ts
@@ -3,12 +3,22 @@ import { CommonModule } from '@angular/common';
 import { AdminLayoutComponent } from './layout.component';
 import { SidebarComponent } from './sidebar/sidebar.component';
 import { SharedModule } from '@shared/shared.module';
-import { LayoutModule } from '../../../layout/layout.module';
+import { HeaderComponent } from './default/header/header.component';
+import { HeaderFullScreenComponent } from './default/header/components/fullscreen.component';
+import { HeaderIconComponent } from './default/header/components/icon.component';
+import { HeaderNotifyComponent } from './default/header/components/notify.component';
+import { HeaderStorageComponent } from './default/header/components/storage.component';
+import { HeaderUserComponent } from './default/header/components/user.component';
 
 const COMPONENTS = [
   AdminLayoutComponent,
-
-  SidebarComponent
+  SidebarComponent,
+  HeaderComponent,
+  HeaderFullScreenComponent,
+  HeaderIconComponent,
+  HeaderNotifyComponent,
+  HeaderStorageComponent,
+  HeaderUserComponent
 ];
 
 @NgModule({
@@ -18,7 +28,6 @@ const COMPONENTS = [
   imports: [
     CommonModule,
     SharedModule,
-    LayoutModule
   ],
   exports: [],
   providers: [],
