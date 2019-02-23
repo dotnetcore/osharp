01c679a09c4a42212c8ae0e2e391db018a76086e
郭明锋 <gmf520@yeah.net>
2019-02-24 03:06:57 +0800

    添加构建脚本 build.bat

--- build.bat
+++ build.bat
@@ -1,2 +1,7 @@
 dotnet restore
 dotnet build
+cd tests/web/ui/ng-alain
+ng build --prod --aot --output-path ../../Liuliu.Demo.Web/wwwroot
+cd ../../Liuliu.Demo.Web
+dotnet build -c Release
+dotnet publish -c Release
