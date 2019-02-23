19a2ea458d03d98f70cdf1da0d8f86d3404f836f
郭明锋 <gmf520@yeah.net>
2019-02-24 03:16:58 +0800

    更新 build.bat脚本

--- build.bat
+++ build.bat
@@ -1,6 +1,7 @@
 dotnet restore
 dotnet build
 cd tests/web/ui/ng-alain
+npm install
 ng build --prod --aot --output-path ../../Liuliu.Demo.Web/wwwroot
 cd ../../Liuliu.Demo.Web
 dotnet build -c Release
