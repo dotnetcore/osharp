621c7604abede11cae25fa9bc57797e5c6e4f325
郭明锋 <gmf520@yeah.net>
2019-02-24 04:15:09 +0800

    删除build.bat

--- build.bat
+++
@@ -1,8 +0,0 @@
-dotnet restore
-dotnet build
-cd tests/web/ui/ng-alain
-npm install
-ng build --prod --aot --output-path ../../Liuliu.Demo.Web/wwwroot
-cd ../../Liuliu.Demo.Web
-dotnet build -c Release
-dotnet publish -c Release
