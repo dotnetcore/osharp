880abfa660c685c028942342917ea9a794526997
郭明锋 <gmf520@yeah.net>
2019-05-04 11:15:26 +0800

    更新 ng-alain 版本到 7.3.2

--- src/OSharp/Core/Modules/IModuleHandler.cs
+++ src/OSharp/Core/Modules/IModuleHandler.cs
@@ -10,7 +10,7 @@
 namespace OSharp.Core.Modules
 {
     /// <summary>
-    /// 定义模块信息处理器
+    /// 定义模块信息处理器，负责在系统初始化时从程序集获取最新的模块信息，并同步到数据库中
     /// </summary>
     public interface IModuleHandler
     {
