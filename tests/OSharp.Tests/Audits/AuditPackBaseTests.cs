25d23b4ed66c0c3e4551483ad3b317dd64c887cd
郭明锋 <gmf520@yeah.net>
2020-03-30 00:37:12 +0800

    :zap: (ut) 添加Audit单元测试

---
+++ tests/OSharp.Tests/Audits/AuditPackBaseTests.cs
@@ -0,0 +1,36 @@
+﻿using Xunit;
+using OSharp.Audits;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Microsoft.Extensions.DependencyInjection;
+
+using OSharp.Core.Packs;
+
+using Shouldly;
+
+
+namespace OSharp.Audits.Tests
+{
+    public class AuditPackBaseTests
+    {
+        [Fact()]
+        public void AddServicesTest()
+        {
+            IServiceCollection services = new ServiceCollection();
+            AuditPack pack = new AuditPack();
+            pack.Level.ShouldBe(PackLevel.Application);
+            pack.Order.ShouldBe(0);
+            pack.AddServices(services);
+            services.Count.ShouldNotBe(0);
+            services.ShouldNotBeEmpty();
+            services.ShouldContain(m => m.ServiceType == typeof(AuditEntityEventHandler) && m.Lifetime == ServiceLifetime.Transient);
+        }
+
+
+        class AuditPack : AuditPackBase
+        { }
+    }
+}
---
+++ tests/OSharp.Tests/Authorization/EntityInfos/EntityInfoBaseTests.cs
@@ -0,0 +1,43 @@
+﻿using Xunit;
+using OSharp.Authorization.EntityInfos;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using OSharp.Json;
+
+using Shouldly;
+
+
+namespace OSharp.Authorization.EntityInfos.Tests
+{
+    public class EntityInfoBaseTests
+    {
+        [Fact]
+        public void PropertiesTest()
+        {
+            EntityInfo entity = new EntityInfo();
+            entity.Properties.ShouldNotBeNull();
+            entity.Properties.ShouldBeEmpty();
+
+            EntityProperty prop = new EntityProperty() { Name = "Name", TypeName = typeof(string).FullName };
+            entity.PropertyJson = prop.ToJsonString();
+            entity.Properties.ShouldNotBeNull();
+            entity.Properties.ShouldBeEmpty();
+
+            entity.PropertyJson = new List<EntityProperty>(){prop}.ToJsonString();
+            entity.Properties.ShouldNotBeEmpty();
+            entity.Properties.Length.ShouldBe(1);
+            entity.Properties.First().Name.ShouldBe(prop.Name);
+            entity.Properties.First().TypeName.ShouldBe(prop.TypeName);
+        }
+
+
+        [Fact()]
+        public void FromTypeTest()
+        {
+            //Assert.True(false, "This test needs an implementation");
+        }
+    }
+}
