a12091c257d16acc2e84758cdbbcd3f2f23ed0e9
郭明锋 <gmf520@yeah.net>
2020-06-16 11:14:38 +0800

    1. 引入monent实现前端时间格式化

---
+++ samples/web/Liuliu.Demo.Mvc/wwwroot/js/osharp.js
@@ -0,0 +1,13 @@
+/**
+ * @Name osharp
+ * @Description 封装OSharp框架需要的前端工具
+ * @Homepage //www.osharp.org
+ * @Author 郭明锋
+ * @License apache 2.0 
+ */
+!function(){
+  "use strict";
+var osharp=function(){
+  this.version="3.1.5;"
+}
+}
