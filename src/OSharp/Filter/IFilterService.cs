f61ae87aab2f3a9df178e99522b5112f318c072d
qiang dai <guanguanchangyu@outlook.com>
2020-11-09 21:31:14 +0800

    ggcy-添加额外过滤条件服务函数

--- src/OSharp/Filter/IFilterService.cs
+++ src/OSharp/Filter/IFilterService.cs
@@ -8,6 +8,7 @@
 // -----------------------------------------------------------------------
 
 using System;
+using System.Collections.Generic;
 using System.Linq.Expressions;
 
 using OSharp.Authorization;
@@ -28,6 +29,15 @@ namespace OSharp.Filter
         /// <param name="group">查询条件组，如果为null，则直接返回 true 表达式</param>
         /// <returns>查询表达式</returns>
         Expression<Func<T, bool>> GetExpression<T>(FilterGroup group);
+
+        /// <summary>
+        /// 获取指定字段和条件组表达式
+        /// </summary>
+        /// <typeparam name="T">表达式实体类型</typeparam>
+        /// <param name="group">查询条件组，如果为null，则直接返回 true 表达式</param>
+        /// <param name="filterfields">查询过滤字段集合</param>
+        /// <returns>查询表达式</returns>
+        Expression<Func<T, bool>> GetExpression<T>(FilterGroup group, IDictionary<string, string> filterfields);
 
         /// <summary>
         /// 获取指定查询条件的查询表达式
