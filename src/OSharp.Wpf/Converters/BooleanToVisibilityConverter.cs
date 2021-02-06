74d41784c1e65e862e6ac04c59b839be997a0240
郭明锋 <gmf520@yeah.net>
2021-02-06 11:19:54 +0800

    增加一些WPF的Converter

--- src/OSharp.Wpf/Converters/BooleanToVisibilityConverter.cs
+++ src/OSharp.Wpf/Converters/BooleanToVisibilityConverter.cs
@@ -1,4 +1,13 @@
-﻿using System;
+﻿// -----------------------------------------------------------------------
+//  <copyright file="BooleanToVisibilityConverter.cs" company="OSharp开源团队">
+//      Copyright (c) 2014-2021 OSharp. All rights reserved.
+//  </copyright>
+//  <site>http://www.osharp.org</site>
+//  <last-editor>郭明锋</last-editor>
+//  <last-date>2021-02-05 1:24</last-date>
+// -----------------------------------------------------------------------
+
+using System;
 using System.Globalization;
 using System.Windows;
 using System.Windows.Data;
@@ -6,7 +15,7 @@ using System.Windows.Data;
 
 namespace OSharp.Wpf.Converters
 {
-    public class VisibilityConverter : IValueConverter
+    public class BooleanToVisibilityConverter : IValueConverter
     {
         /// <summary>
         /// 转换值。
@@ -17,7 +26,7 @@ namespace OSharp.Wpf.Converters
         /// <param name="value">绑定源生成的值。</param><param name="targetType">绑定目标属性的类型。</param><param name="parameter">要使用的转换器参数。</param><param name="culture">要用在转换器中的区域性。</param>
         public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
         {
-            bool flag = (bool)value;
+            bool flag = value != null && (bool)value;
             return flag ? Visibility.Visible : Visibility.Collapsed;
         }
 
@@ -30,7 +39,8 @@ namespace OSharp.Wpf.Converters
         /// <param name="value">绑定目标生成的值。</param><param name="targetType">要转换到的类型。</param><param name="parameter">要使用的转换器参数。</param><param name="culture">要用在转换器中的区域性。</param>
         public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
         {
-            throw new NotImplementedException();
+            Visibility? visibility = (Visibility?)value;
+            return visibility == Visibility.Visible;
         }
     }
 }
