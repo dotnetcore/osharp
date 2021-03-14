a5f443813beb4aed1ebcf58bf19d391ea7ae947d
郭明锋 <mf.guo@qq.com>
2021-03-14 21:57:00 +0800

    添加`IKeyGenerator`，`ISequentialGuidGenerator`接口，用于实体主键数据的生成

---
+++ src/OSharp/Entity/KeyGenerate/ISequentialGuidGenerator.cs
@@ -0,0 +1,27 @@
+﻿// -----------------------------------------------------------------------
+//  <copyright file="ISequentialGuidGenerator.cs" company="OSharp开源团队">
+//      Copyright (c) 2014-2021 OSharp. All rights reserved.
+//  </copyright>
+//  <site>http://www.osharp.org</site>
+//  <last-editor>郭明锋</last-editor>
+//  <last-date>2021-03-14 13:48</last-date>
+// -----------------------------------------------------------------------
+
+using System;
+
+using OSharp.Data;
+
+
+namespace OSharp.Entity.KeyGenerate
+{
+    /// <summary>
+    /// 定义有顺序的Guid主键生成器
+    /// </summary>
+    public interface ISequentialGuidGenerator : IKeyGenerator<Guid>
+    {
+        /// <summary>
+        /// 获取 顺序Guid数据库类型
+        /// </summary>
+        DatabaseType DatabaseType { get; }
+    }
+}
---
+++ src/OSharp/Entity/KeyGenerate/SnowKeyGenerator.cs
@@ -0,0 +1,39 @@
+﻿// -----------------------------------------------------------------------
+//  <copyright file="SnowKeyGenerator.cs" company="OSharp开源团队">
+//      Copyright (c) 2014-2021 OSharp. All rights reserved.
+//  </copyright>
+//  <site>http://www.osharp.org</site>
+//  <last-editor>郭明锋</last-editor>
+//  <last-date>2021-03-14 13:50</last-date>
+// -----------------------------------------------------------------------
+
+using OSharp.Data.Snows;
+
+
+namespace OSharp.Entity.KeyGenerate
+{
+    /// <summary>
+    /// 雪花算法主键生成器
+    /// </summary>
+    public class SnowKeyGenerator : IKeyGenerator<long>
+    {
+        private readonly IIdGenerator _idGenerator;
+
+        /// <summary>
+        /// 初始化一个<see cref="SnowKeyGenerator"/>类型的新实例
+        /// </summary>
+        public SnowKeyGenerator(IIdGenerator idGenerator)
+        {
+            _idGenerator = idGenerator;
+        }
+
+        /// <summary>
+        /// 获取一个<see cref="long"/>类型的主键数据
+        /// </summary>
+        /// <returns></returns>
+        public long Create()
+        {
+            return _idGenerator.NewLong();
+        }
+    }
+}
