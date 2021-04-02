9ca2f3fadb790456674be8200ae2836350891235
郭明锋 <gmf520@yeah.net>
2021-04-03 01:03:40 +0800

    feat(tenancy): 添加多租户项目OSharp.MultiTenancy，OSharp.MultiTenancy.EntityFrameworkCore

---
+++ src/OSharp.MultiTenancy.EntityFrameworkCore/MultiTenancyEntityFrameworkCorePack.cs
@@ -0,0 +1,33 @@
+﻿// -----------------------------------------------------------------------
+//  <copyright file="MultiTenancyEntityFrameworkCorePack.cs" company="OSharp开源团队">
+//      Copyright (c) 2014-2021 OSharp. All rights reserved.
+//  </copyright>
+//  <site>http://www.osharp.org</site>
+//  <last-editor>郭明锋</last-editor>
+//  <last-date>2021-04-03 0:52</last-date>
+// -----------------------------------------------------------------------
+
+using System.ComponentModel;
+
+using Microsoft.Extensions.DependencyInjection;
+
+
+namespace OSharp.MultiTenancy.EntityFrameworkCore
+{
+    /// <summary>
+    /// 多租户EFCore模块
+    /// </summary>
+    [Description("多租户EFCore模块")]
+    public class MultiTenancyEntityFrameworkCorePack : MultiTenancyPackBase
+    {
+        /// <summary>
+        /// 将模块服务添加到依赖注入服务容器中
+        /// </summary>
+        /// <param name="services">依赖注入服务容器</param>
+        /// <returns></returns>
+        public override IServiceCollection AddServices(IServiceCollection services)
+        {
+            return base.AddServices(services);
+        }
+    }
+}
---
+++ src/OSharp.MultiTenancy/MultiTenancyPackBase.cs
@@ -0,0 +1,24 @@
+﻿// -----------------------------------------------------------------------
+//  <copyright file="MultiTenancyPack.cs" company="OSharp开源团队">
+//      Copyright (c) 2014-2021 OSharp. All rights reserved.
+//  </copyright>
+//  <site>http://www.osharp.org</site>
+//  <last-editor>郭明锋</last-editor>
+//  <last-date>2021-04-03 0:45</last-date>
+// -----------------------------------------------------------------------
+
+using System.ComponentModel;
+
+using OSharp.AspNetCore;
+using OSharp.Core.Packs;
+
+
+namespace OSharp.MultiTenancy
+{
+    /// <summary>
+    /// 多租户模块基类
+    /// </summary>
+    [DependsOnPacks(typeof(AspNetCorePack))]
+    public abstract class MultiTenancyPackBase : OsharpPack
+    { }
+}
