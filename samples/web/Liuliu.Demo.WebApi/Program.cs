3872c5c7dc9b5f00e714f23c819059ec343fae4c
郭明锋 <gmf520@yeah.net>
2021-12-06 14:58:40 +0800

    sdk: 按照.net6更新Program.cs

--- samples/web/Liuliu.Demo.WebApi/Program.cs
+++ samples/web/Liuliu.Demo.WebApi/Program.cs
@@ -1,4 +1,4 @@
-﻿// -----------------------------------------------------------------------
+// -----------------------------------------------------------------------
 //  <copyright file="Program.cs" company="OSharp开源团队">
 //      Copyright (c) 2014-2020 OSharp. All rights reserved.
 //  </copyright>
@@ -7,6 +7,7 @@
 //  <last-date>2020-06-02 11:32</last-date>
 // -----------------------------------------------------------------------
 
+using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Hosting;
 
@@ -17,14 +18,32 @@ namespace Liuliu.Demo.Web
     {
         public static void Main(string[] args)
         {
+#if NET6_0_OR_GREATER
+            CreateApplication(args).Run();
+#else
             CreateHostBuilder(args).Build().Run();
+#endif
         }
 
+#if NET6_0_OR_GREATER
+
+        public static WebApplication CreateApplication(string[] args)
+        {
+            var builder = WebApplication.CreateBuilder(args);
+            Startup startup = new Startup();
+            startup.ConfigureServices(builder.Services);
+            var app = builder.Build();
+            startup.Configure(app, app.Environment);
+            return app;
+        }
+#else
         public static IHostBuilder CreateHostBuilder(string[] args) =>
             Host.CreateDefaultBuilder(args)
                 .ConfigureWebHostDefaults(webBuilder =>
                 {
                     webBuilder.UseStartup<Startup>();
                 });
+
+#endif
     }
 }
