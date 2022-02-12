42696c99c17435d656969a1913da20363d6c2ec6
郭明锋 <gmf520@yeah.net>
2022-02-13 01:15:04 +0800

    nuget.build脚本添加powershell版本限制

---
+++ tests/Liuliu.Demo.Core.Tests/CoreTest.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using AsmResolver.DotNet;
+
+using Xunit;
+
+
+namespace Liuliu.Demo.Core.Tests
+{
+    public class CoreTest
+    {
+        [Fact]
+        public void Test01()
+        {
+            string path = "Liuliu.Demo.Core.dll";
+            var moduleDef = ModuleDefinition.FromFile(path);
+            foreach (TypeDefinition typeDef in moduleDef.GetAllTypes())
+            {
+                
+            }
+        }
+    }
+}
