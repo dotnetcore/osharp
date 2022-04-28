616586b5f356690cf67eba0cbc1ba480fd178a8a
郭明锋 <gmf520@yeah.net>
2022-04-28 11:45:41 +0800

    移除ng-alain项目

--- samples/web/ui-clients/ng-alain8/src/style-icons.ts
+++
@@ -1,5 +0,0 @@
-// Custom icon static resources
-
-import { BellOutline, BorderOuterOutline, BulbOutline, CloseCircleOutline, DashboardOutline, DesktopOutline, ExceptionOutline, InfoOutline, LinkOutline, MailOutline, ProfileOutline, SafetyOutline, SecurityScanOutline } from "@ant-design/icons-angular/icons";
-
-export const ICONS = [ BellOutline, BorderOuterOutline, BulbOutline, CloseCircleOutline, DashboardOutline, DesktopOutline, ExceptionOutline, InfoOutline, LinkOutline, MailOutline, ProfileOutline, SafetyOutline, SecurityScanOutline ];
