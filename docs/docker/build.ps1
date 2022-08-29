c4095bec25e090ee4c5c3080421c97932d744444
郭明锋 <gmf520@yeah.net>
2022-08-29 18:23:22 +0800

    docs: 更新osharp.docs构建脚本

--- docs/docker/build.ps1
+++ docs/docker/build.ps1
@@ -1,12 +1,11 @@
 ﻿$version = Read-Host "请输入版本号"
-docker build -t mkdocs-material:$($version) .
-docker tag mkdocs-material:$($version) gmf520/mkdocs-material:$($version)
-docker tag mkdocs-material:$($version) gmf520/mkdocs-material:latest
+docker build -t gmf520/mkdocs-material:$version .
+docker tag gmf520/mkdocs-material:$version gmf520/mkdocs-material:latest
 Write-Host "构建完成: gmf520/mkdocs-material:$version"
-Write-Host "即将发布到dockerhub，请输入账号密码"
+Write-Host "即将发布到dockerhub, 请输入账号密码"
 $username = Read-Host "请输入账号"
 $password = Read-Host "请输入密码"
 docker login -u $username -p $password
-docker push gmf520/mkdocs-material:$($version)
+docker push gmf520/mkdocs-material:$version
 docker push gmf520/mkdocs-material:latest
-Write-Host ("构建并发布成功: gmf520/mkdocs-material:$($version)")
+Write-Host "发布完成: gmf520/mkdocs-material:$version"
