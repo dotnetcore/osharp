a3a25e5a1c84fcd1e048713cc1e3e7895f7364c9
郭明锋 <gmf520@yeah.net>
2023-09-11 15:21:15 +0800

    add test

---
+++ tests/OSharp.Tests/Authorization/DataAuthServiceTests.cs
@@ -0,0 +1,14 @@
+using Microsoft.Extensions.DependencyInjection;
+
+
+namespace OSharp.Authorization.Tests
+{
+    public class DataAuthServiceTests
+    {
+        public DataAuthServiceTests()
+        {
+            IServiceCollection services = new ServiceCollection();
+            
+        }
+    }
+}
