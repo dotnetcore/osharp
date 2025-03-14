9c7175096f936089a3520ac7800424a59dab4f8d
shiningrise <shiningrise@qq.com>
2025-03-14 09:18:41 +0800

    Yitter.IdGenerator 加分布式支持

--- samples/web/Liuliu.Demo.WebApi/Startups/Yitter/YitterIdGeneratorPack.cs
+++ samples/web/Liuliu.Demo.WebApi/Startups/Yitter/YitterIdGeneratorPack.cs
@@ -13,27 +13,132 @@ using Yitter.IdGenerator;
 using Liuliu.Demo.Web.Startups.Yitter;
 using OSharp.Entity.KeyGenerate;
 using System.Configuration;
+using Microsoft.Extensions.Options;
+using OSharp.Exceptions;
+using Microsoft.Extensions.Caching.Distributed;
+using Microsoft.DotNet.Scaffolding.Shared;
 
 namespace Liuliu.Demo.Web.Startups
 {
+    [DependsOnPacks(typeof(OsharpCorePack))]
     public class YitterIdGeneratorPack : OsharpPack
     {
+        private const string MainLockName = "sys_idGen:workerId:lock";
+        private const string MainValueKey = "sys_idGen:workerId:value";
+
         public YitterIdGeneratorPack()
         {
         }
 
+        public override PackLevel Level => PackLevel.Framework;
+
         public override IServiceCollection AddServices(IServiceCollection services)
         {
-            using (var serviceProvider = services.BuildServiceProvider())
-            {
-                var _configuration = serviceProvider.GetRequiredService<IConfiguration>();
-                // 配置 IdGeneratorOptions
-                services.Configure<IdGeneratorOptions>(_configuration.GetSection("IdGeneratorOptions"));
-            }
-                
+            IConfiguration configuration = services.GetConfiguration();
+            services.Configure<IdGeneratorOptions>(configuration.GetSection("IdGeneratorOptions"));               
 
             services.Replace<IKeyGenerator<long>, YitterSnowKeyGenerator>(ServiceLifetime.Singleton);
             return services;
+        }
+
+        override public void UsePack(IServiceProvider provider)
+        {
+            var _options = provider.GetRequiredService<IOptions<IdGeneratorOptions>>().Value;
+            var _logger = provider.GetRequiredService<ILogger<YitterIdGeneratorPack>>();
+            var workerId = GetWorkerId(provider, _options);
+            var options = new IdGeneratorOptions
+            {
+                Method = _options.Method,
+                BaseTime = _options.BaseTime,
+                WorkerId = workerId,
+                WorkerIdBitLength = _options.WorkerIdBitLength,
+                SeqBitLength = _options.SeqBitLength,
+                MaxSeqNumber = _options.MaxSeqNumber,
+                MinSeqNumber = _options.MinSeqNumber,
+                TopOverCostCount = _options.TopOverCostCount
+            };
+            
+            _logger.LogInformation($"Yitter.IdGenerator已配置，WorkerId: {workerId}");
+            YitIdHelper.SetIdGenerator(options);
+        }
+
+        private ushort GetWorkerId(IServiceProvider provider,IdGeneratorOptions options)
+        {
+            var lockName = $"{MainLockName}";
+            var valueKey = $"{MainValueKey}";
+
+            var minWorkId = 0;
+            var maxWorkId = (int)Math.Pow(2, (double)options.WorkerIdBitLength);
+
+            var cache = provider.GetRequiredService<IDistributedCache>();
+            var _logger = provider.GetRequiredService<ILogger<YitterIdGeneratorPack>>();
+
+            long workId = -1;
+            var tempWorkIds = Enumerable.Range(minWorkId, maxWorkId).Select(id => id.ToString()).ToList();
+            try
+            {
+                string workIdKey = "";
+                foreach (var item in tempWorkIds)
+                {
+                    var workIdStr = item;
+                    workIdKey = $"{valueKey}:{workIdStr}";
+                    var exist = cache.Get<bool>(workIdKey);
+                    if (exist)
+                    {
+                        workIdKey = "";
+                        continue;
+                    }
+
+                    _logger.LogInformation($"############ 当前应用雪花WorkId:【{workIdStr}】############");
+
+                    workId = long.Parse(workIdStr);
+                    if (workId < minWorkId || workId > maxWorkId)
+                        continue;
+
+                    // 设置雪花Id算法机器码
+                    YitIdHelper.SetIdGenerator(new IdGeneratorOptions
+                    {
+                        WorkerId = (ushort)workId,
+                        WorkerIdBitLength = options.WorkerIdBitLength,
+                        SeqBitLength = options.SeqBitLength
+                    });
+
+                    var cacheOptions = new DistributedCacheEntryOptions
+                    {
+                        AbsoluteExpirationRelativeToNow = TimeSpan.FromSeconds(60)
+                    };
+                    cache.Set(workIdKey, true, cacheOptions);
+
+                    break;
+                }
+
+                if (string.IsNullOrWhiteSpace(workIdKey)) throw new OsharpException("未设置有效的机器码，启动失败");
+
+                // 开一个任务设置当前workId过期时间
+                Task.Run(() =>
+                {
+                    while (true)
+                    {
+                        var cacheOptions = new DistributedCacheEntryOptions
+                        {
+                            AbsoluteExpirationRelativeToNow = TimeSpan.FromSeconds(60)
+                        };
+                        cache.Set(workIdKey, true, cacheOptions);
+
+                        Thread.Sleep(10000);
+                    }
+                });
+            }
+            catch (Exception ex)
+            {
+                throw new OsharpException($"{ex.Message};{ex.StackTrace};{ex.StackTrace}");
+            }
+            finally
+            {
+
+            }
+            if(workId < minWorkId || workId >= maxWorkId) throw new OsharpException("未设置有效的机器码，启动失败");
+            return (ushort)workId;
         }
     }
 }
--- samples/web/Liuliu.Demo.WebApi/Startups/Yitter/YitterSnowKeyGenerator.cs
+++ samples/web/Liuliu.Demo.WebApi/Startups/Yitter/YitterSnowKeyGenerator.cs
@@ -9,32 +9,9 @@ namespace Liuliu.Demo.Web.Startups.Yitter
 {
     public class YitterSnowKeyGenerator : IKeyGenerator<long>
     {
-        private readonly IdGeneratorOptions _options;
-        private readonly ILogger<YitterSnowKeyGenerator> _logger;
 
-        public YitterSnowKeyGenerator(IOptions<IdGeneratorOptions> options, ILogger<YitterSnowKeyGenerator> logger)
+        public YitterSnowKeyGenerator()
         {
-            _logger = logger;   
-            _options = options.Value;
-            InitializeIdGenerator();
-        }
-
-        private void InitializeIdGenerator()
-        {
-            var options = new IdGeneratorOptions
-            {
-                Method = _options.Method,
-                BaseTime = _options.BaseTime,
-                WorkerId = _options.WorkerId,
-                WorkerIdBitLength = _options.WorkerIdBitLength,
-                SeqBitLength = _options.SeqBitLength,
-                MaxSeqNumber = _options.MaxSeqNumber,
-                MinSeqNumber = _options.MinSeqNumber,
-                TopOverCostCount = _options.TopOverCostCount
-            };
-            var _workerId = _options.WorkerId;
-            _logger.LogInformation($"Yitter.IdGenerator已配置，WorkerId: {_workerId}");
-            YitIdHelper.SetIdGenerator(options);
         }
 
         public long Create()
